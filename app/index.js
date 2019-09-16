cfad7d78ba046a2607086da663b7f3a030bff323
Gboyega Dada <boyega@gmail.com>
2019-09-16 21:41:46 +0100

    It's alive! It's alive!  👾👾

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                ✔
+            </div>
+        )
+    }
+}
+
+ReactDom.render(
+    <App />, 
+    document.getElementById('app')
+)
