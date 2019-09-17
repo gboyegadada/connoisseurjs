4e05b6f16b67cbe4c9ed65dba0c135f2bbd7fdc5
Gboyega Dada <boyega@gmail.com>
2019-09-17 07:57:48 +0100

    Add redux_setup/ folder

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
+import {Provider} from 'react-redux';
+import makeStore from './redux_setup/store'
+
+export const store = makeStore();
 
 class App extends React.Component {
 
     render() {
         return (
-            <div>
-                ✔
-            </div>
+            <Provider store={store}>
+                <div>
+                    ✔
+                </div>
+            </Provider>
         )
     }
 }
