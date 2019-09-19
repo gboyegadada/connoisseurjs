7ff885bfe275480297f9e2871efecc8b1e209b37
Gboyega Dada <boyega@gmail.com>
2019-09-19 14:43:52 +0100

    Add README and install mucho lib

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/3aba018f-2451-4b74-be60-5245fdc74be0/deploy-status)](https://app.netlify.com/sites/jovial-golick-376158/deploys)
+
+## Coveo Take Home Assignment
--- test/test.tsx
+++ test/test.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import sinon from 'sinon';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 import Header from '../app/components/Header';
 
 describe('<Header />', () => {
   it('renders three <header /> elements', () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper.find(<header/>)).to.have.lengthOf(1);
+    expect(wrapper.find('header')).to.have.length(1);
   });
 });
