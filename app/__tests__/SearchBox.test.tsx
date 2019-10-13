aeede5a4777796a123a52b15f788e087e5d8ac73
Gboyega Dada <boyega@gmail.com>
2019-10-13 21:59:56 +0100

    Add test for <SearchBox />

---
+++ app/__tests__/SearchBox.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+import SearchBox from "../components/SearchBox";
+
+
+describe("<SearchBox />", () => {
+  test("should display clickable Facet Value", async () => {
+    const main = shallow(<SearchBox 
+        searchAction={(): any => null } 
+        updateSearchQueryAction={(): any => null } 
+        defaultQuery={{
+          q: 'Vin Rouge',
+          aq: '',
+        }} 
+        queryParams={{
+          q: 'Vin Rouge',
+          aq: '',
+        }} 
+        status={1}
+        />)
+
+    expect(main).toMatchSnapshot()
+  });
+});
